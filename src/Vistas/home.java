11277136d724e1a72bbbe74509c942ae23c63bf8
karlaRubi1502 <karrufela@gmail.com>
2022-03-23 12:32:08 -0600

    Hice los botones OJO NO SIRVEN

--- src/Vistas/home.java
+++ src/Vistas/home.java
@@ -28,17 +28,21 @@ public class home extends javax.swing.JFrame {
 
         jPanel1 = new javax.swing.JPanel();
         btnAdmin = new javax.swing.JButton();
-        jButton2 = new javax.swing.JButton();
+        btnAspi = new javax.swing.JButton();
 
         setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
 
+        jPanel1.setBackground(new java.awt.Color(255, 255, 255));
+
+        btnAdmin.setBackground(new java.awt.Color(255, 255, 255));
         btnAdmin.setIcon(new javax.swing.ImageIcon(getClass().getResource("/Imagenes/admin64.png"))); // NOI18N
         btnAdmin.setText("Administrador");
         btnAdmin.setBorder(null);
+        btnAdmin.setBorderPainted(false);
+        btnAdmin.setContentAreaFilled(false);
         btnAdmin.setHorizontalTextPosition(javax.swing.SwingConstants.CENTER);
         btnAdmin.setPressedIcon(new javax.swing.ImageIcon(getClass().getResource("/Imagenes/admin64.png"))); // NOI18N
         btnAdmin.setSelectedIcon(new javax.swing.ImageIcon(getClass().getResource("/Imagenes/admin96.png"))); // NOI18N
-        btnAdmin.setVerticalAlignment(javax.swing.SwingConstants.BOTTOM);
         btnAdmin.setVerticalTextPosition(javax.swing.SwingConstants.BOTTOM);
         btnAdmin.addActionListener(new java.awt.event.ActionListener() {
             public void actionPerformed(java.awt.event.ActionEvent evt) {
@@ -46,38 +50,56 @@ public class home extends javax.swing.JFrame {
             }
         });
 
-        jButton2.setText("jButton2");
+        btnAspi.setIcon(new javax.swing.ImageIcon(getClass().getResource("/Imagenes/aspi64.png"))); // NOI18N
+        btnAspi.setText("Aspirante");
+        btnAspi.setActionCommand("");
+        btnAspi.setBorder(null);
+        btnAspi.setBorderPainted(false);
+        btnAspi.setContentAreaFilled(false);
+        btnAspi.setHorizontalTextPosition(javax.swing.SwingConstants.CENTER);
+        btnAspi.setPressedIcon(new javax.swing.ImageIcon(getClass().getResource("/Imagenes/aspi64.png"))); // NOI18N
+        btnAspi.setRolloverIcon(new javax.swing.ImageIcon(getClass().getResource("/Imagenes/aspi96.png"))); // NOI18N
+        btnAspi.setSelectedIcon(new javax.swing.ImageIcon(getClass().getResource("/Imagenes/aspi96.png"))); // NOI18N
+        btnAspi.setVerticalTextPosition(javax.swing.SwingConstants.BOTTOM);
+        btnAspi.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                btnAspiActionPerformed(evt);
+            }
+        });
 
         javax.swing.GroupLayout jPanel1Layout = new javax.swing.GroupLayout(jPanel1);
         jPanel1.setLayout(jPanel1Layout);
         jPanel1Layout.setHorizontalGroup(
             jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
             .addGroup(jPanel1Layout.createSequentialGroup()
-                .addGap(51, 51, 51)
-                .addComponent(btnAdmin, javax.swing.GroupLayout.PREFERRED_SIZE, 121, javax.swing.GroupLayout.PREFERRED_SIZE)
-                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, 195, Short.MAX_VALUE)
-                .addComponent(jButton2, javax.swing.GroupLayout.PREFERRED_SIZE, 115, javax.swing.GroupLayout.PREFERRED_SIZE)
-                .addGap(92, 92, 92))
+                .addGap(37, 37, 37)
+                .addComponent(btnAdmin, javax.swing.GroupLayout.PREFERRED_SIZE, 155, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addGap(84, 84, 84)
+                .addComponent(btnAspi, javax.swing.GroupLayout.PREFERRED_SIZE, 153, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addContainerGap(javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
         );
         jPanel1Layout.setVerticalGroup(
             jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
             .addGroup(jPanel1Layout.createSequentialGroup()
-                .addGap(65, 65, 65)
-                .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING, false)
-                    .addComponent(jButton2, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
-                    .addComponent(btnAdmin, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
-                .addContainerGap(87, Short.MAX_VALUE))
+                .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.TRAILING)
+                    .addGroup(jPanel1Layout.createSequentialGroup()
+                        .addContainerGap()
+                        .addComponent(btnAspi, javax.swing.GroupLayout.PREFERRED_SIZE, 171, javax.swing.GroupLayout.PREFERRED_SIZE))
+                    .addGroup(javax.swing.GroupLayout.Alignment.LEADING, jPanel1Layout.createSequentialGroup()
+                        .addGap(47, 47, 47)
+                        .addComponent(btnAdmin, javax.swing.GroupLayout.PREFERRED_SIZE, 137, javax.swing.GroupLayout.PREFERRED_SIZE)))
+                .addContainerGap(22, Short.MAX_VALUE))
         );
 
         javax.swing.GroupLayout layout = new javax.swing.GroupLayout(getContentPane());
         getContentPane().setLayout(layout);
         layout.setHorizontalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addComponent(jPanel1, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+            .addComponent(jPanel1, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
         );
         layout.setVerticalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addComponent(jPanel1, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+            .addComponent(jPanel1, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
         );
 
         pack();
@@ -86,6 +108,10 @@ public class home extends javax.swing.JFrame {
     private void btnAdminActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_btnAdminActionPerformed
         // TODO add your handling code here:
     }//GEN-LAST:event_btnAdminActionPerformed
+
+    private void btnAspiActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_btnAspiActionPerformed
+        // TODO add your handling code here:
+    }//GEN-LAST:event_btnAspiActionPerformed
 
     /**
      * @param args the command line arguments
@@ -124,7 +150,7 @@ public class home extends javax.swing.JFrame {
 
     // Variables declaration - do not modify//GEN-BEGIN:variables
     private javax.swing.JButton btnAdmin;
-    private javax.swing.JButton jButton2;
+    private javax.swing.JButton btnAspi;
     private javax.swing.JPanel jPanel1;
     // End of variables declaration//GEN-END:variables
 }
